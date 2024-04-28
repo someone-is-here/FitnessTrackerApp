ba2afa56d88246bb8546ee4c29a16049457abd78
Tatsiana Shurko <tatsiana_shurko@Tatsianas-MacBook-Air.local>
2024-04-29 00:41:12 +0300

    Add settings screen

---
+++ FitnessTrackerApp/app/src/main/java/com/example/fitnesstrackerapp/mvvm/fragments/signing/ResetPasswordFragment.kt
@@ -0,0 +1,81 @@
+package com.example.fitnesstrackerapp.mvvm.fragments.signing
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.fitnesstrackerapp.R
+import com.example.fitnesstrackerapp.databinding.FragmentResetPasswordBinding
+import com.example.fitnesstrackerapp.other.Signing
+
+class ResetPasswordFragment : Signing() {
+
+    private var binding: FragmentResetPasswordBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+
+        return binding!!.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpHandlers()
+        setAnimation(context)
+    }
+    override fun setAnimation(context: Context?){
+        super.setAnimation(context)
+
+        binding!!.tvResetPassword.startAnimation(topToBottomAnimation)
+        binding!!.ivSettings.startAnimation(settingsAnimation)
+
+        binding!!.tvSignUp.startAnimation(scaleAnimation)
+        binding!!.tvSignIn.startAnimation(scaleAnimation)
+    }
+    private fun setUpHandlers() {
+
+        binding!!.tvSignIn.setOnClickListener {
+            findNavController().navigate(R.id.action_resetPasswordFragment_to_signInFragment)
+        }
+
+        binding!!.tvSignUp.setOnClickListener{
+            findNavController().navigate(R.id.action_resetPasswordFragment_to_signUpFragment)
+        }
+
+        binding!!.ivSettings.setOnClickListener{
+            findNavController().navigate(R.id.action_resetPasswordFragment_to_appSettingsFragment);
+        }
+
+        binding!!.btnResetPassword.setOnClickListener {
+            val email = binding!!.etEmail.text.toString()
+
+            if(checkInput(email)) {
+                firebaseAuth.sendPasswordResetEmail(email).addOnSuccessListener {
+                    Toast.makeText(activity, requireContext().getString(R.string.email_sent), Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.action_resetPasswordFragment_to_signInFragment)
+                }.addOnFailureListener {
+                    Toast.makeText(activity, requireContext().getString(R.string.email_sending_failed), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun checkInput(email: String):Boolean {
+        if (email.isEmpty()){
+            binding!!.etEmail.error = requireContext().getString(R.string.email_field_empty)
+            return false
+        }else if(!isValidEmail(email)){
+            binding!!.etEmail.error = requireContext().getString(R.string.email_invalid)
+            return false
+        }
+
+        return true
+    }
+}
